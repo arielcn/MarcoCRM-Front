0b6d447d347d698a89181872ae1124122915a6fc
arielcn <arielcohen.179@gmail.com>
2023-10-02 12:03:26 -0300

    cambie pocas cosas

--- src/Pages/Reuniones.js
+++ src/Pages/Reuniones.js
@@ -6,45 +6,47 @@ const Reuniones = () => {
       <div class="row gx-5">
         <section class="col-4">
           <div class="p-3">
-            <div className="row">
-              <div class="card mb-3 col-sm-6" id="cardFormato">
+            <div className="row text-center">
+              <div class="card mb-3 me-3 col-sm-5" id="cardFormato">
                 <img src="../../telefono.png" class="card-img-top" alt=""></img>
-                  <div class="card-body">
-                    <h5 class="card-title">Llamada</h5>
-                  </div>
-              </div>              
-              <div class="card mb-3 col-sm-6" id="cardFormato">
-                <img src="../../gmail.png" class="card-img-top" alt=""></img>
-                  <div class="card-body">
-                    <h5 class="card-title">Mail</h5>
-                  </div>
-              </div>                          
-              <div class="card mb-3 col-sm-6" id="cardFormato">
+                <div class="card-body">
+                  <h5 class="card-title">Llamada</h5>
+                </div>
+              </div>
+              <div class="card mb-3 col-sm-5" id="cardFormato">
+                <img src="../../gmail.png" height="100%" class="card-img-top" alt=""></img>
+                <div class="card-body">
+                  <h5 class="card-title">Mail</h5>
+                </div>
+              </div>
+              <div class="card mb-3 me-3 col-sm-5" id="cardFormato">
                 <img src="../../whatsapp.png" class="card-img-top" alt=""></img>
-                  <div class="card-body">
-                    <h5 class="card-title">WhatsApp</h5>
-                  </div>
-              </div>                           
-              <div class="card mb-3 col-sm-6" id="cardFormato">
+                <div class="card-body">
+                  <h5 class="card-title">WhatsApp</h5>
+                </div>
+              </div>
+              <div class="card mb-3 col-sm-5" id="cardFormato">
                 <img src="../../videollamada.png" class="card-img-top" alt=""></img>
-                  <div class="card-body">
-                    <h5 class="card-title">Videollamada</h5>
-                  </div>
-              </div>              
+                <div class="card-body">
+                  <h5 class="card-title">Videocall</h5>
+                </div>
+              </div>
 
               <div className="row fechaHora">
                 <img src='../../reloj.png' alt=''></img>
-                <h1>Fecha y hora</h1>
-                <label>Fecha: <input type='date'></input></label>
-                <label>Hora: <input type='time'></input></label>
+                <div>
+                  <h3>Fecha y hora</h3>
+                  <label>Fecha: <input type='date'></input></label>
+                  <label>Hora: <input type='time'></input></label>
+                </div>
               </div>
             </div>
           </div>
         </section>
-        
+
         <section class="col-8">
-          <h3>Nombre cliente</h3>
-          <div class="p-3">
+          <div class="p-3 text-white">
+            <h3>Nombre cliente</h3>
             <h1>Título de la reunion</h1>
           </div>
         </section>
