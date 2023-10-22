c414f42c95755bd5cc8c81e63da0f05c93143a0c
arielcn <arielcohen.179@gmail.com>
2023-10-22 18:51:17 -0300

    boton volver atras agregado

--- src/Layout.js
+++ src/Layout.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { Outlet, useNavigate } from "react-router-dom";
 
-import { Outlet } from "react-router-dom";
+
+
+
 const Layout = () => {
+    const navigate = useNavigate();
+
+    const goBack = () => {
+        navigate(-1);
+    };
     return (
         <>
             <nav>
                 <header className="App-header">
+                    <div className='iconoFlecha'>
+                        <ion-icon name="arrow-back-outline" onClick={goBack} size='large'></ion-icon>
+                    </div>
                     <img src="logo.png" className="App-logo" alt="logo" />
                 </header>
             </nav>
