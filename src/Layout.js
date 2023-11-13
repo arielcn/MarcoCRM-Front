4a2ccc455708f1e18865dec14553a0d2ec911aa1
MARTIN ARIEL PEREZ <you@example.com>
2023-11-13 11:58:15 -0300

    editar perfil boton

--- src/Layout.js
+++ src/Layout.js
@@ -3,13 +3,16 @@ import { Outlet, useNavigate } from "react-router-dom";
 
 
 
-
 const Layout = () => {
     const navigate = useNavigate();
 
     const goBack = () => {
         navigate(-1);
     };
+
+    const click = () => {
+        navigate('/editar-perfil')
+    }
     return (
         <>
             <nav>
@@ -18,6 +21,8 @@ const Layout = () => {
                         <ion-icon name="arrow-back-outline" onClick={goBack} size='large'></ion-icon>
                     </div>
                     <img src="logo.png" className="App-logo" alt="logo" />
+                    <ion-icon name="person-circle-outline" className='iconoPerfil' onClick={click} size='48px'></ion-icon>
+                    
                 </header>
             </nav>
             <Outlet />
